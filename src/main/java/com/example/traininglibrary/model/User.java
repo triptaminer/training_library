4969dc270423fdf57fb15132bb1fcc43214df87c
triptaminer <triptaminer@gmail.com>
2025-03-03 20:47:30 +0100

    db changed to h2

--- src/main/java/com/example/traininglibrary/model/User.java
+++ src/main/java/com/example/traininglibrary/model/User.java
@@ -1,8 +1,10 @@
 package com.example.traininglibrary.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "book_user")
 public class User extends Person {
 
     private String email;
